d1a2e30e45ae8b28aa5d3ad6726e46f979376ff3
mio manns <miomanns265@gmail.com>
2022-08-16 17:52:45 +0200

    finished script

--- typeracer/script.js
+++ typeracer/script.js
@@ -1,13 +1,40 @@
 /*
 Usage:
     paste this file inside your console before the race starts
-
-    or run:
-    var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/iluvpy/internal-js/main/typeracer/script.js';
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
 */ 
 
-console.log("Loaded script!");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+    // wait until game starts
+    while (true) {
+        
+        var timeLeft = document.getElementsByClassName("timeDisplay")[0].firstChild.textContent;
+        if (timeLeft.length > 3) break;
+        await sleep(100);
+    }
+    console.log("game started!");
+
+    // get text 
+    var text = "";
+    var spans = document.querySelectorAll("span[unselectable=on]");
+    for (var span of spans) {
+        text += span.textContent;
+    } 
+    console.log(text);
+    sleep(1000);
+    var input = document.getElementsByClassName("txtInput")[0];
+    console.log("writing..");
+    for (var char of text) {
+        input.value += char;
+        await sleep(50);
+        input.click();
+    }
+
+}
+
+main();
+
 
