c933309ed88c801ca085fe6d3baadf7b692c04e8
mio manns <miomanns265@gmail.com>
2022-08-16 15:50:30 +0200

    added complete usage

--- typeracer/script.js
+++ typeracer/script.js
@@ -3,6 +3,11 @@ Usage:
     paste this file inside your console before the race starts
 
     or run:
-    
+    var script = document.createElement('script');
+    script.src = 'https://raw.githubusercontent.com/iluvpy/internal-js/main/typeracer/script.js';
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
 */ 
+
+console.log("Loaded script!");
 
